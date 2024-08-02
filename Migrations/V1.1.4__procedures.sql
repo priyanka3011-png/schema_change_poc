7998b3e4f2dfa0783006c265b474997929fd7cdb
Priyanka Nemana <priyanka.nemana@rndc-usa.com>
2024-08-02 16:51:10 +0530

    view

---
+++ Migrations/V1.1.4__procedures.sql
@@ -0,0 +1,14 @@
+USE SCHEMA DEMO;
+
+CREATE or replace PROCEDURE proc3()
+  RETURNS VARCHAR
+  LANGUAGE javascript
+  AS
+  $$
+  var rs = snowflake.execute( { sqlText: 
+      `INSERT INTO HELLO_WORLD ('column 1','column2',45);`
+       } );
+  return 'Done.';
+  $$;
+
+  CREATE VIEW POC_VIEW AS select AGE FROM HELLO_WORLD; 
